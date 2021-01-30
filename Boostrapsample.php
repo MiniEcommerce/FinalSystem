684bfb3f648bfee96148657271ccf2ecdaa9a4d6
EudesSilerio <silerioeudesaugustine@gmail.com>
2021-01-30 15:32:57 +0700

    Homepage

---
+++ Boostrapsample.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+
+<div class="jumbotron text-center">
+  <h1>Game Kiosk</h1>
+  <p>The premier PS4 game store</p> 
+</div>
+
+<div class="row">
+    <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="IMG/CODMWF.png" target="_blank">
+        <img class="img-responsive" src="IMG/CODMWF.png" alt="Call Of Duty: Modern Warefare" width="460" height="345">
+          <div class="caption">
+          <h3>Call Of Duty: Modern Warefare</h3>
+          <p>(Game Description)</p>
+          <p>P3000.00</p>
+          </div>
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="IMG/FFVIIRemake.jpg" target="_blank">
+        <img class="img-responsive" src="IMG/FFVIIRemake.png" alt="Final Fantasy VII (Remake)" width="460" height="345">
+          <div class="caption">
+          <h3>Final Fantasy VII (Remake)</h3>
+          <p>(Game Description)</p>
+          <p>P7000.00</p>
+          </div>
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="IMG/Tekken.png" target="_blank">
+        <img class="img-responsive" src="IMG/Tekken.png" alt="Tekken 7" width="460" height="345">
+          <div class="caption">
+          <h3>Tekken 7</h3>        
+          <p>(Game Description)</p>
+          <p>P3000.00</p>
+            <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
+          </div>
+        </a>
+      </div>
+    </div>
+
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+
+    </div>
+    <div class="col-sm-4">
+
+    </div>
+    <div class="col-sm-4">
+
+    </div>
+  </div>
+</div>
+
+
+</body>
+</html>
