0e2c2066908b7de8543da1d8f8626667cbfeccca
EudesSilerio <silerioeudesaugustine@gmail.com>
2021-02-06 15:23:25 +0700

    Update02/06

--- Boostrapsample.php
+++ Boostrapsample.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>GameKiosk</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
@@ -9,19 +9,43 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
-<body>
+<body style="height:1500px">
+
+<! --Navigation Bar--> 
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+        <a href="IMG/Logo.png" target="_blank">
+        <img class="img-responsive" src="IMG/Logo.png" alt="GameKioskLogo" width="180" height="180">
+        </a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Shop</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Log-out</a></li>
+    </ul>
+  </div>
+</nav>
 
 
+
+<! --Home--> 
 <div class="jumbotron text-center">
   <h1>Game Kiosk</h1>
   <p>The premier PS4 game store</p> 
 </div>
 
+
+
+<! --Shop--> 
+<div class="jumbotron text-center">
+  <h1>Products</h1>
 <div class="row">
     <div class="col-md-4">
       <div class="thumbnail">
         <a href="IMG/CODMWF.png" target="_blank">
-        <img class="img-responsive" src="IMG/CODMWF.png" alt="Call Of Duty: Modern Warefare" width="460" height="345">
+        <img class="img-responsive" src="IMG/CODMWF.png" alt="Call Of Duty: Modern Warefare" width="470" height="478">
           <div class="caption">
           <h3>Call Of Duty: Modern Warefare</h3>
           <p>(Game Description)</p>
@@ -33,7 +57,7 @@
     <div class="col-md-4">
       <div class="thumbnail">
         <a href="IMG/FFVIIRemake.jpg" target="_blank">
-        <img class="img-responsive" src="IMG/FFVIIRemake.png" alt="Final Fantasy VII (Remake)" width="460" height="345">
+        <img class="img-responsive" src="IMG/FFVIIRemake.png" alt="Final Fantasy VII (Remake)" width="363" height="478">
           <div class="caption">
           <h3>Final Fantasy VII (Remake)</h3>
           <p>(Game Description)</p>
@@ -45,32 +69,36 @@
     <div class="col-md-4">
       <div class="thumbnail">
         <a href="IMG/Tekken.png" target="_blank">
-        <img class="img-responsive" src="IMG/Tekken.png" alt="Tekken 7" width="460" height="345">
+        <img class="img-responsive" src="IMG/Tekken.png" alt="Tekken 7" width="363" height=478>
           <div class="caption">
           <h3>Tekken 7</h3>        
           <p>(Game Description)</p>
           <p>P3000.00</p>
-            <p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
           </div>
         </a>
       </div>
     </div>
-
-
-<div class="container">
-  <div class="row">
-    <div class="col-sm-4">
-
-    </div>
-    <div class="col-sm-4">
-
-    </div>
-    <div class="col-sm-4">
-
-    </div>
-  </div>
 </div>
 
+    <! --About--> 
+
+<div class="jumbotron text-center">
+<div class="col-md-4">
+      <div class="thumbnail">
+        <a href="IMG/ben-neale-zpxKdH_xNSI-unsplash.jpg" target="_blank">
+        <img class="img-responsive" src="IMG/ben-neale-zpxKdH_xNSI-unsplash.jpg" alt="Tekken 7" width="480" height=480>
+        </a>
+      </div>
+    </div>
+  <h2>About Game Kiosk</h2> 
+  <p>Dorime, Ameno</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+  ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+  culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
 
 </body>
 </html>
