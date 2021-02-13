9eb238141c0c41d0dbd0e9f656ca1c1958ee087d
EudesSilerio <silerioeudesaugustine@gmail.com>
2021-02-13 12:42:12 +0700

    02/13 Update

--- Boostrapsample.php
+++ Boostrapsample.php
@@ -1,44 +1,159 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="assets/fontawesome/css/all.css">
+
+    <title>GameKiosk</title>
+  </head>
+  <body>
+    <!--NAVIGATION BAR-->
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="#"><img src="IMG/Logo.png" width="80" height="80" class="img fluid"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <li style = "margin-right: 10px"> <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          <li class="nav-item">
+            <li style = "margin-right: 10px"> <a class="nav-link" href="#">Shop </a>
+          <li class="nav-item">
+            <li style = "margin-right: 10px"> <a class="nav-link" href="#">About </a>
+           <li class="nav-item">
+            <li style = "margin-right: 10px"> <a class="nav-link" href="#">Log-out </a> 
+          <!--
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          </li>
+          <li>
+            
+                <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+          </li>
+        -->
+        </ul>
+      </div>
+    </nav>
+
+    <!--HOMEPAGE-->
+    <div class="container-fluid mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                      <img src="IMG/carl-raw-m3hn2Kn5Bns-unsplash.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                      <img src="IMG/ben-neale-zpxKdH_xNSI-unsplash.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                      <img src="IMG/carl-raw-8Gdayy2Lhi0-unsplash.jpg" class="d-block w-100" alt="...">
+                    </div>
+                  </div>
+                  <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
+                </div>
+            </div>
+        </div>
+
+        <!-- PRODUCTS-->
+        <hr>
+        <div class="row">
+            <div class="col-md-9 offset-md-3">
+                <div class="h3">Products</div>
+                <div class="row row-cols-1 row-cols-md-4">
+                  <div class="col mb-4">
+                    <div class="card h-100">
+                      <img src="IMG/CODMWF.png" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h5 class="card-title">Call Of Duty: Modern Warefare</h5>
+                        <p class="card-text txt-color">P3000.00</p>
+
+                        <div class="btn btn-danger w-100">Learn more</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col mb-4">
+                    <div class="card h-100">
+                      <img src="IMG/FFVIIRemake.png" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h5 class="card-title">Final Fantasy VII (Remake)</h5>
+                        <p class="card-text txt-color">P7000.00</p>
+
+                        <div class="btn btn-danger w-100">Learn more</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col mb-4">
+                    <div class="card h-100">
+                      <img src="IMG/Tekken.png" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h5 class="card-title">Tekken 7</h5>
+                        <p class="card-text txt-color">P3000.00</p>
+
+                        <div class="btn btn-danger w-100">Learn more</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+<!--
+<head>
   <title>GameKiosk</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="style.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body style="height:1500px">
-
-<! --Navigation Bar--> 
-<nav class="navbar navbar-inverse navbar-fixed-top">
+<! --Navigation Bar 
+<nav class="navbar navbar-inverse fixed-top bg-dark">
   <div class="container-fluid">
-    <div class="navbar-header">
-        <a href="IMG/Logo.png" target="_blank">
-        <img class="img-responsive" src="IMG/Logo.png" alt="GameKioskLogo" width="180" height="180">
-        </a>
+  <div class = "navbar-header">
+      <a class = "navbar-brand">
+        <img class="img-responsive" src="IMG/Logo.png" alt="GameKioskLogo" width="80" height="80">
+      </a>
     </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Shop</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Log-out</a></li>
+    <ul class = "nav">
+      <li style = "margin-right: 10px"><a href="#">Home</a></li>
+      <li style = "margin-right: 10px"><a href="#">Shop</a></li>
+      <li style = "margin-right: 10px"><a href="#">About</a></li>
+      <li style = "margin-right: 10px"><a href="#">Log-out</a></li>
     </ul>
+    
   </div>
 </nav>
-
-
-
-<! --Home--> 
+<! --Home
 <div class="jumbotron text-center">
   <h1>Game Kiosk</h1>
   <p>The premier PS4 game store</p> 
 </div>
 
-
-
-<! --Shop--> 
+<! --Shop 
 <div class="jumbotron text-center">
   <h1>Products</h1>
 <div class="row">
@@ -81,7 +196,6 @@
 </div>
 
     <! --About--> 
-
 <div class="jumbotron text-center">
 <div class="col-md-4">
       <div class="thumbnail">
@@ -98,7 +212,39 @@
   reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
   pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
   culpa qui officia deserunt mollit anim id est laborum.</p>
-</div>
 
-</body>
+<!--ABOUT US-->
+          <div class="row pt-3 text-light" id="footer">
+            
+              <h2>About The Team</h2>
+              
+            <div class="col-md-8 offset-md-2">
+                <div class="row">
+                    <div class="col-md-4">Joshua Concepcion(Programmer)</div>
+                    <div class="col-md-4">Rey Vincent Dolz(Programmer)</div>
+                    <div class="col-md-4">Eudes Augustine Silerio(Designer)</div>
+                </div>
+            </div>
+        </div>
+        <div class="row text-light pt-3" id="foot">
+            <div class="col-md-8 offset-md-2">
+                <div class="row">
+                    <div class="col-md-4"><img src="img/IMG/Logo.png" width="80" height="80" class="img fluid"></div>
+                    <div class="col-md-4 mt-2"><center>2021 by Game Kiosk. A project in Integrative Programming</center></div>
+                    <div class="col-md-4">
+                        <i class="fab fa-facebook-square" style="font-size: 50px;"></i>
+                        <i class="fas fa-envelope" style="font-size: 50px;"></i>
+                        <i class="fab fa-discord" style="font-size: 50px;"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="js/jquery-3.5.1.slim.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+
+  </body>
 </html>
+
